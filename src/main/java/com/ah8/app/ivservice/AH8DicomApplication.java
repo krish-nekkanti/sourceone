bebabbfbf11199ee2295c700af046d51772785fb
Admin <Admin@Temp-KP.chinasystems-me.com>
2018-06-11 10:55:37 +0400

    comment removed

--- src/main/java/com/ah8/app/ivservice/AH8DicomApplication.java
+++ src/main/java/com/ah8/app/ivservice/AH8DicomApplication.java
@@ -11,8 +11,6 @@ public class AH8DicomApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(AH8DicomApplication.class, args);
     }
-    
-    // Used when deploying to a standalone servlet container
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(AH8DicomApplication.class);
